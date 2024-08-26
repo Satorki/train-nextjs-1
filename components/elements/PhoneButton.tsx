ef046c8a5b6a533e91e51e77a3c2f14d6de001cf
Satorki <sebastian.kedzier@gmail.com>
2024-08-26 14:22:46 +0200

    svgr uninstalled, phonebutton for navbar styled

--- components/elements/PhoneButton.tsx
+++ components/elements/PhoneButton.tsx
@@ -1,23 +1,27 @@
-import React from "react";
-import PhoneSvg from "../svgs/PhoneSvg";
-
+import { SlPhone } from "react-icons/sl";
 const PhoneButton = () => {
   return (
     <div className="phoneButton">
-      <span>
-        <PhoneSvg />
-      </span>
+      <SlPhone className="phoneIcon" />
       <p>000 000 000</p>
       <style jsx>
         {`
+          .phoneIcon {
+            color: var(--first-color);
+            font-size: 2rem;
+            margin-bottom: 5px;
+          }
           .phoneButton {
             display: flex;
-            flex-direction: column;
             align-items: center;
-            justify-content: center;
+            justify-content: space-between;
+            gap: 0.8rem;
             color: var(--first-color);
             font-family: "SpaceGrotesk";
             font-size: 1.1rem;
+            border: 1px solid var(--first-color);
+            border-radius: 25px;
+            padding: 2px 10px;
           }
         `}
       </style>
